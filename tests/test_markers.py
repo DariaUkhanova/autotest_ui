966711bdcee42e0847f850ff75d3971511cc78b9
DariaUkhanova <daria_ukhanova@mail.ru>
2025-09-23 00:09:09 +0300

    Маркировки Pytest

---
+++ tests/test_markers.py
@@ -0,0 +1,22 @@
+import pytest
+
+
+@pytest.mark.smoke
+def test_smoke_case():
+    ...
+
+def test_regression_case():
+    ...
+
+@pytest.mark.regression
+class TestUserAuthentication:
+    @pytest.mark.smoke
+    def test_login(self):
+        ...
+
+    @pytest.mark.slow
+    def test_password_reset(self):
+        ...
+
+    def test_logout(self):
+        ...
