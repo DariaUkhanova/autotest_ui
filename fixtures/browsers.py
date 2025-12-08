8cea5622bcee1b30318d6807e904cd6655f00331
DariaUkhanova <daria_ukhanova@mail.ru>
2025-12-08 23:14:17 +0300

    Параметризация pytest

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -8,7 +8,7 @@ def chromium_page(playwright: Playwright) -> Page:
     browser.close()
 
 @pytest.fixture(scope='session')
-def initialize_browser_state(playwright: Playwright) -> Page:
+def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
@@ -36,6 +36,6 @@ def initialize_browser_state(playwright: Playwright) -> Page:
 def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state='browser-state.json')
-    page = context.new_page()
-    yield page
+
+    yield context.new_page()
     browser.close()
