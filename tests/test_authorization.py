718637c318a48904b3f29f19d894da32e3ba5e04
DariaUkhanova <daria_ukhanova@mail.ru>
2025-12-12 23:43:20 +0300

    Параметризация pytest

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -14,10 +14,10 @@ def test_wrong_email_or_password_authorization(chromium_page: Page, email, passw
     chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
 
     email_input = chromium_page.get_by_test_id("login-form-email-input").locator('input')
-    email_input.fill('user.name@gmail.com')
+    email_input.fill(password)
 
     password_input = chromium_page.get_by_test_id("login-form-password-input").locator('input')
-    password_input.fill('Password')
+    password_input.fill(email)
 
     login_button = chromium_page.get_by_test_id("login-page-login-button")
     login_button.click()
